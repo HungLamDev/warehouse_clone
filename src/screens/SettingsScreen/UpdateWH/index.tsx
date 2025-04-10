d240c543993eb29c3dba132f85fdc73dc71c913e
HungLamDev <hunglam.dev@gmail.com>
2025-04-10 16:19:01 +0700

    done Wh

--- src/screens/SettingsScreen/UpdateWH/index.tsx
+++ src/screens/SettingsScreen/UpdateWH/index.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import FullScreenContainerWithNavBar from "../../../components/FullScreenContainerWithNavBar";
-import EditableTableOrigin from "../../../components/EditableTable"; // Import wrapper
+
 import MyButton from "../../../components/MyButton";
 import InputField from "../../../components/InputField";
 import { connect_string } from "../../LoginScreen/ChooseFactory";
@@ -13,17 +13,16 @@ import GenericAutocomplete from "../../../components/GenericAutocomplete";
 import TableOrigin from "../../../components/TableOrigin";
 import ModalCofirm from "../../../components/ModalConfirm";
 
+
 const UpdateWH = () => {
     const { t } = useTranslation();
     const dataUser = useSelector((state: any) => state.UserLogin.user);
-
     const [rows, setRows] = useState<any[]>([]);
-    const [Rack, setRack] = useState("");
     const [MaterialType, setMaterialType] = useState("");
     const [disable, setDisable] = useState(false);
     const [loading, setLoading] = useState(false);
-    const [color, setColor] = useState(false);
     const [listWH, setListWH] = useState<any[]>([]);
+    const [selectedWHToAll, setSelectedWHToAll] = useState<any>(null);
     const [selectedWH, setSelectedWH] = useState<any>(null);
     const [openCofirm, setOpenCofirm] = useState(false)
     const [cofirmType, setCofirmType] = useState('')
@@ -31,32 +30,26 @@ const UpdateWH = () => {
     const [RackInput, setRackInput] = useState("");
     const [RackList, setRackList] = useState<string[]>([]);
 
-
-    const handleRack = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRack(event.target.value);
-    };
     const handleMaterialType = (event: React.ChangeEvent<HTMLInputElement>) => {
         setMaterialType(event.target.value);
     };
     const handleRackInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const rawValue = event.target.value;
         setRackInput(rawValue);
-
         const racks = rawValue
             .split(/\s+/)            // tách theo khoảng trắng, tab, newline
             .map(s => s.trim())      // xóa khoảng trắng 2 bên
             .filter(Boolean);        // loại bỏ chuỗi rỗng
 
-        setRackList(racks);
-        console.log("list", racks)
+        setRackList(racks)
     };
 
     const columns: any[] = [
         { field: "stt", headerName: t("dcpNum") as string, width: 160, headerClassName: "custom-header" },
         { field: "Rack", headerName: t("dcpRack") as string, width: 1000, headerClassName: "custom-header" },
-        { field: "Position", headerName: "Position" as string, width: 160, headerClassName: "custom-header" },
-        { field: "Material_Type", headerName: "Material_Type" as string, width: 300, headerClassName: "custom-header" },
-        { field: "WH", headerName: "WH", width: 160, headerClassName: "custom-header", selected: true },
+        { field: "Position", headerName: t("dcpPosition") as string, width: 160, headerClassName: "custom-header" },
+        { field: "Material_Type", headerName: t("dcmUser_ID") as string, width: 300, headerClassName: "custom-header" },
+        { field: "WH", headerName: t("lblChooseWH"), width: 160, headerClassName: "custom-header", selected: true },
     ];
 
     useEffect(() => {
@@ -72,13 +65,12 @@ const UpdateWH = () => {
         setDisable(true);
         let url = "";
         let data = {};
-        console.log("selectedWH:", selectedWH);
-        console.log("selectedWH?.value:", selectedWH?.value);
-            url = connect_string + "api/get_data_storeage";
-            data = { RackList: RackList || "",
-                 Material_Type: MaterialType || "",
-                WH: selectedWH || ""};
-            console.log("data view" ,data)
+        url = connect_string + "api/get_data_storeage";
+        data = {
+            RackList: RackList || "",
+            Material_Type: MaterialType || "",
+            WH: selectedWH || ""
+        }
         try {
             const response = await axios.post(url, data, config);
             const arr = response.data.map((item: any, index: number) => ({
@@ -106,12 +98,11 @@ const UpdateWH = () => {
     };
 
     const handleRefresh = () => {
-        setRack("");
         setMaterialType("");
         setRows([]);
-        setListWH([]);
         setSelectedWH(null);
         setRackInput("")
+        setRackList([])
 
     };
 
@@ -130,15 +121,10 @@ const UpdateWH = () => {
         const url = connect_string + "api/Get_WH_From_Data_Storage";
         try {
             const res = await axios.post(url);
-            const data = res.data.map((wh: string) => ({
-                value: wh,
-                title: wh,
-            }));
+           
             setListWH(res.data);
             setSelectedWH(null)
-            console.log("data", data)
-            console.log("res", res.data)
-
+            setSelectedWHToAll(null)
         } catch (err) {
             console.error("Lỗi khi lấy WH:", err);
         } finally {
@@ -146,43 +132,9 @@ const UpdateWH = () => {
         }
     };
 
-
-
-    // const handleupdateWH = async () => {
-    //     if (rows.length > 0) {
-    //         setLoading(true)
-    //         const url = connect_string + ""
-    //         const data =
-    //         {
-    //             chxTransition: "false",
-    //             List_update: rows,
-    //             saFactory: dataUser[0].factoryName,
-    //             saVersion: dataUser[0].WareHouse,
-    //             USERID_PWA: dataUser[0].UserId,
-    //         }
-    //         console.log("data", data)
-    //         console.log("data rows", rows)
-    //         axios.post(url, data, config).then(response => {
-    //             if (response.data === true) {
-    //                 setColor(true)
-    //             }
-    //             else {
-    //                 setColor(false)
-    //             }
-    //             setOpenCofirm(false)
-    //         }).finally(() => {
-    //             setLoading(false)
-    //         })
-    //     }
-
-    // };
-   
     const handleupdateWH = async () => {
-        console.log("row", rows)
-        console.log("originalRows", originalRows)
         if (rows.length > 0) {
             setLoading(true);
-
             const changedRows = rows.filter((row, index) => {
                 const originalRow = originalRows[index];
                 if (!originalRow) return true;
@@ -191,15 +143,12 @@ const UpdateWH = () => {
                     return row[key] !== originalRow[key];
                 });
             });
-
             if (changedRows.length === 0) {
-                alert("Không có thay đổi nào để cập nhật.");
+                handleOpenConfirm('NoChange')
                 setLoading(false);
                 return;
             }
-
-            const url = connect_string + "api/update_Data_Storage"; 
-
+            const url = connect_string + "api/update_Data_Storage";
             const data = {
                 List_update: changedRows,
                 saFactory: dataUser[0].factoryName,
@@ -209,86 +158,111 @@ const UpdateWH = () => {
 
             try {
                 const response = await axios.post(url, data, config);
-                setColor(response.data === true);
+                console.log("response",response)
                 if (response.data === true) {
-                    alert("Cập nhật thành công!");
+                    handleOpenConfirm('success')
                 } else {
-                    alert("Cập nhật thất bại!");
+                    handleOpenConfirm('error')
                 }
             } catch (error) {
                 console.error("Lỗi cập nhật:", error);
-                alert("Đã xảy ra lỗi khi cập nhật.");
             } finally {
-                setOpenCofirm(false);
                 setLoading(false);
             }
         }
     };
-
-
-
-
+    const handleApplyWHToAll = (WH: any) => {
+        const updatedRows = rows.map(row => ({
+            ...row,
+            WH,
+        }));
+        setRows(updatedRows);
+    };
     return (
         <FullScreenContainerWithNavBar
             hidden={true}
             sideBarDisable={true}
             sideBarNavigate="/update-WH"
             title={t("lblupdate_inforWH")}
-            navigate="/"
-        >
+            navigate="/">
             <Box paddingX={1} paddingBottom={1} className={"dark-bg-secondary border-bottom-white"}>
                 <Stack>
                     <Grid container paddingTop={"16px"} justifyContent={"center"}>
-                        <Grid item xs={1} display={"flex"} justifyContent={"start"} alignItems={"center"}>
-                            <Typography className="textsize">RACK</Typography>
+                        <Grid item xs={0.5} display={"flex"} justifyContent={"start"} alignItems={"center"}>
+                            <Typography className="textsize">{t("dcpRack")}</Typography>
                         </Grid>
                         <Grid item xs={2} display={"flex"} paddingRight={"16px"}>
                             <InputField value={RackInput} handle={handleRackInputChange} disable={disable} />
                         </Grid>
                         <Grid item xs={1.2} display={"flex"} justifyContent={"start"} alignItems={"center"}>
-                            <Typography className="textsize">Material Type</Typography>
+                            <Typography className="textsize">{t("dcmUser_ID")}</Typography>
                         </Grid>
                         <Grid item xs={2} display={"flex"} paddingRight={"16px"}>
                             <InputField value={MaterialType} handle={handleMaterialType} disable={disable} />
                         </Grid>
-                        <Grid item xs={0.5} display={"flex"} justifyContent={"start"} alignItems={"center"}>
-                            <Typography className="textsize">WH</Typography>
+                       
+                           <Grid item xs={1} display={"flex"} justifyContent={"start"} alignItems={"center"}>
+                            <Typography className="textsize"> {t("lblChooseWH")}</Typography>
                         </Grid>
                         <Grid item display={"flex"} alignItems={"center"} xs={1.5} paddingRight={"16px"}>
                             <GenericAutocomplete
                                 options={listWH || []}
                                 value={selectedWH}
-                                onChange={(newValue: any | null) => setSelectedWH(newValue)}
+                                onChange={(newValue: any | null) => {
+                                    setSelectedWH(newValue)
+                                }}
                                 getOptionLabel={(option) =>
                                     typeof option === 'string' ? option : option?.title || ''
                                 }
                                 isOptionEqualToValue={(option, value) => option.value === value?.value}
                             />
-
                         </Grid>
-
+                        
                     </Grid>
                     <Grid container padding={"16px"} justifyContent={"center"}>
-
                         <Grid item paddingRight={"16px"}>
                             <MyButton name={t("btnSearch")} onClick={handleSearch} disabled={disable} />
                         </Grid>
                         <Grid item paddingRight={"16px"}>
-                            <MyButton name={t("Lưu")} disabled={disable} onClick={() => rows.length > 0 && handleOpenConfirm('confirm')} />
+                            <MyButton name={t("btnSave")} disabled={disable} onClick={() => rows.length > 0 && handleOpenConfirm('confirm')} />
                         </Grid>
-                        <Grid item>
+                        <Grid item paddingRight={"16px"}>
                             <MyButton name={t("btnClean") as string} disabled={disable} onClick={handleRefresh} />
                         </Grid>
+                       
+                        {rows.length > 2 && (
+                            <>
+                                <Grid item xs={1} display={"flex"} justifyContent={"start"} alignItems={"center"}>
+                                    <Typography className="textsize">{t("rbtUpdateManyRack")}</Typography>
+                                </Grid>
+                                <Grid item display={"flex"} alignItems={"center"} xs={1.5} paddingRight={"16px"}>
+                                    <GenericAutocomplete
+                                        options={listWH || []}
+                                        value={selectedWHToAll}
+                                        onChange={(newValue: any | null) => {
+                                            setSelectedWHToAll(newValue);
+                                            handleApplyWHToAll(newValue);
+                                        }}
+                                        getOptionLabel={(option) =>
+                                            typeof option === 'string' ? option : option?.title || ''
+                                        }
+                                        isOptionEqualToValue={(option, value) => option.value === value?.value}
+                                    />
+                                </Grid>
+                            </>
+                        )}
+                       
                     </Grid>
                 </Stack>
             </Box>
             <Stack overflow={"hidden"} direction="row" sx={{ height: "100%" }}>
                 <Grid sx={{ width: "100%", overflow: "hidden" }}>
-
-                    <TableOrigin color={color} columns={columns} rows={rows} arrNotShowCell={[]} handleDoubleClick={null} handlerowClick={null} dataSelected={listWH} />
-
+                    <TableOrigin  columns={columns} rows={rows} arrNotShowCell={[]} handleDoubleClick={null} handlerowClick={null} dataSelected={listWH} />
                 </Grid>
                 {cofirmType === 'confirm' && <ModalCofirm onPressOK={handleupdateWH} open={openCofirm} onClose={handleCloseConfirm} title={t("msgYouWantUpdate") as string} />}
+                {cofirmType === 'error' && <ModalCofirm showCancel={false} onPressOK={handleCloseConfirm} open={openCofirm} onClose={handleCloseConfirm} title={t("msgerror") as string} />}
+                {cofirmType === 'NoChange' && <ModalCofirm showCancel={false} onPressOK={handleCloseConfirm} open={openCofirm} onClose={handleCloseConfirm} title={t("msgNochange") as string} />}
+                {cofirmType === 'success' && <ModalCofirm showCancel={false} onPressOK={handleCloseConfirm} open={openCofirm} onClose={handleCloseConfirm} title={t("msgUpdateSuccesful") as string} />}
             </Stack>
         </FullScreenContainerWithNavBar>
     );
